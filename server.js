e42a41fd5ff562c53a13cdc7c0ad38143eaa74e4
Greg Czaplicki <glc3344@gmail.com>
2019-04-19 14:42:33 -0400

    refactor: adjust server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Start server
 // app.listen(serverConfig.port, () => {
