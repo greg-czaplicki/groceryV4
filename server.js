f5eb7320687b703c42c37ae3b5c9a0de826ccaa6
Greg Czaplicki <glc3344@gmail.com>
2019-04-19 15:06:03 -0400

    refactor: for heroku

--- server.js
+++ server.js
@@ -46,12 +46,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-
 // Start server
-// app.listen(serverConfig.port, () => {
-//   console.log(`🚀  Server is running on port: ${serverConfig.port}...`);
-// });
-app.listen(PORT, () => {
-  console.log(`🚀  Server is running on port: ${PORT}...`);
+app.listen(serverConfig.port, () => {
+  console.log(`🚀  Server is running on port: ${serverConfig.port}...`);
 });
