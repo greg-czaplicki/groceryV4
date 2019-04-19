b822ff15bd6d4c9c0b5712a14b059452d89944b5
Greg Czaplicki <glc3344@gmail.com>
2019-04-19 16:43:38 -0400

    refactor

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.use((err, req, res, next) => {
 
 // Server static assets if in PRODUCTION
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
 
-  app.get("*", (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
   });
 }
 
