ce0b8c61771f116b29e88242401cf9366d7adfbf
Greg Czaplicki <glc3344@gmail.com>
2019-04-19 17:52:54 -0400

    refactor

--- config/config.js
+++ config/config.js
@@ -1,7 +1,5 @@
 const config = {
-  mongoURL:
-    "mongodb://greg:greg@cluster0-coxov.mongodb.net/test?retryWrites=true" ||
-    "mongodb://localhost/groceryV4",
+  mongoURI: process.env.MONGO_URI || "mongodb://localhost/groceryV4",
   port: process.env.PORT || 3001
 };
 
