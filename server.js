50ed118e80c082992864cb6c4453f30b1424434e
Greg Czaplicki <glc3344@gmail.com>
2019-04-19 16:59:22 -0400

    refactor

--- server.js
+++ server.js
@@ -20,29 +20,29 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
 // Routes
 app.use("/items", itemsRouter);
 app.use("/categories", categoriesRouter);
 
-// Error handler
-app.use((err, req, res, next) => {
-  const error = app.get("env") === "development" ? err : {};
-  const status = error.status || 500;
+// // Error handler
+// app.use((err, req, res, next) => {
+//   const error = app.get("env") === "development" ? err : {};
+//   const status = error.status || 500;
 
-  res.status(status).json({
-    error: {
-      message: error.message
-    }
-  });
-  console.error(err);
-});
+//   res.status(status).json({
+//     error: {
+//       message: error.message
+//     }
+//   });
+//   console.error(err);
+// });
 
-// Server static assets if in PRODUCTION
-
-app.use(express.static("client/build"));
-
-app.get("*", (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // Start server
