85658bcf43e6ad95e250a19679ccd09e3c1850bd
Greg Czaplicki <glc3344@gmail.com>
2019-04-23 12:49:37 -0400

    refactor: remove react-pose

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import posed from "react-pose";
 
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -11,42 +10,24 @@ import Tooltip from "@material-ui/core/Tooltip";
 
 import { toggleItemComplete } from "../actions/itemActions";
 
-const Swipeable = posed.div({
-  draggable: "x",
-  dragBounds: { right: 0 },
-  dragEnd: {
-    x: 0,
-    y: 0,
-    transition: { type: "spring", stiffness: 250 }
-  }
-});
-
 const Item = ({ item, toggleItemComplete }) => {
   return (
-    <Swipeable
-      onValueChange={{
-        x: x => {
-          if (x <= -180) toggleItemComplete(item);
-        }
-      }}
-    >
-      <ListItem button style={{ height: 57 }}>
-        <ListItemText onClick={() => toggleItemComplete(item)}>
-          {item.name}
-        </ListItemText>
-        <Tooltip title="Edit Item" placement="left">
-          <Fab
-            color="primary"
-            aria-label="Edit"
-            size="small"
-            component={Link}
-            to={`/item/edit/${item._id}`}
-          >
-            <EditIcon />
-          </Fab>
-        </Tooltip>
-      </ListItem>
-    </Swipeable>
+    <ListItem button style={{ height: 57 }}>
+      <ListItemText onClick={() => toggleItemComplete(item)}>
+        {item.name}
+      </ListItemText>
+      <Tooltip title="Edit Item" placement="left">
+        <Fab
+          color="primary"
+          aria-label="Edit"
+          size="small"
+          component={Link}
+          to={`/item/edit/${item._id}`}
+        >
+          <EditIcon />
+        </Fab>
+      </Tooltip>
+    </ListItem>
   );
 };
 
