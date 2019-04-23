4889b31a83715f9b33ae87f6fa12d15128f917f9
Greg Czaplicki <glc3344@gmail.com>
2019-04-23 15:14:58 -0400

    refactor: adjust main container margin

--- client/src/components/styles/container.js
+++ client/src/components/styles/container.js
@@ -2,13 +2,14 @@ const styles = theme => ({
   main: {
     width: "auto",
     display: "block", // Fix IE 11 issue.
-    marginTop: 100,
+    marginTop: 80,
     marginLeft: theme.spacing.unit * 3,
     marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(960 + theme.spacing.unit * 3 * 2)]: {
       width: 800,
       marginLeft: "auto",
-      marginRight: "auto"
+      marginRight: "auto",
+      marginTop: 120
     }
   }
 });
