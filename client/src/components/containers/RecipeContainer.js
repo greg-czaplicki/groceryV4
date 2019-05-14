7ff56456c6dc5cc1a56f0f1c142e271458ceed68
Greg Czaplicki <glc3344@gmail.com>
2019-05-14 14:51:10 -0400

    feat: add styling to recipe container

--- client/src/components/containers/RecipeContainer.js
+++ client/src/components/containers/RecipeContainer.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
+import withStyles from "@material-ui/core/styles/withStyles";
+import containerStyles from "../styles/container";
+
 class RecipeContainer extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.main}>
         <h1>Recipe Container</h1>
       </div>
     );
   }
 }
 
-export default RecipeContainer;
+export default withStyles(containerStyles)(RecipeContainer);
